04da3e30577de118c578bf29f476212ec086e646
Archer <tw7869@gmail.com>
2019-07-24 14:53:00 +0800

    update

--- redis-node-socket-chat/index.js
+++ redis-node-socket-chat/index.js
@@ -19,20 +19,10 @@ fs.readFile('creds.json', 'utf-8', function (err, data) {
     // Redis Client Ready
     client.once('ready', function () {
 
-        // Flush Redis DB
-        // client.flushdb();
-
-        // Initialize Chatters
-        client.get('chat_users', function (err, reply) {
-            if (reply) {
-                chatters = JSON.parse(reply);
-            }
-        });
-
         // Initialize Messages
         client.get('UploadNotification', function (err, reply) {
             if (reply) {
-                chat_messages = JSON.parse(reply);
+                chat_messages = reply;
             }
         });
     });
@@ -44,9 +34,6 @@ var port = process.env.PORT || 8080;
 http.listen(port, function () {
     console.log('Server Started. Listening on *:' + port);
 });
-
-// Store people in chatroom
-var chatters = [];
 
 // Store messages in chatroom
 var chat_messages = [];
@@ -64,12 +51,9 @@ app.get('/', function (req, res) {
     });
 });
 
-
 // API - Send + Store Message
 app.post('/send_message', function (req, res) {
-    var message = req.body.message;
-    chat_messages.push(message);
-    client.set('UploadNotification', JSON.stringify(chat_messages));
+    client.set('UploadNotification', JSON.stringify(req.body));
     res.send({
         'status': 'OK'
     });
@@ -80,23 +64,10 @@ app.get('/get_messages', function (req, res) {
     res.send(chat_messages);
 });
 
-// API - Get Chatters
-app.get('/get_chatters', function (req, res) {
-    res.send(chatters);
-});
-
 // Socket Connection
-// UI Stuff
 io.on('connection', function (socket) {
-
-    // Fire 'send' event for updating Message list in UI
+    // Fire 'UploadNotification' event for updating Message list in UI
     socket.on('message', function (data) {
-        io.emit('send', data);
+        io.emit('UploadNotification', data);
     });
-
-    // Fire 'count_chatters' for updating Chatter Count in UI
-    socket.on('update_chatter_count', function (data) {
-        io.emit('count_chatters', data);
-    });
-
 });
--- redis-node-socket-chat/public/js/main.js
+++ redis-node-socket-chat/public/js/main.js
@@ -1,29 +1,5 @@
 $(function () {
-
-    // test
     var socket = io();
-    var chatter_count;
-    $.get('/get_chatters', function (response) {
-        
-        chatter_count = response.length; //update chatter count
-        if (response.status == 'OK') { //username doesn't already exists
-            socket.emit('update_chatter_count', {
-                'action': 'increase'
-            });
-            $('#leave-chat').data('username', username);
-            $('#send-message').data('username', username);
-            $.get('/get_messages', function (response) {
-                if (response.length > 0) {
-                    var message_count = response.length;
-                    var html = '';
-                    for (var x = 0; x < message_count; x++) {
-                        html += "<div class='msg'><div class='user'>" + response[x]['sender'] + "</div><div class='txt'>" + response[x]['message'] + "</div></div>";
-                    }
-                    $('.messages').html(html);
-                }
-            })
-        }
-    });
     
     $('#send-message').click(function () {
         var message = $.trim($('#message').val());
@@ -31,30 +7,16 @@ $(function () {
             url: '/send_message',
             type: 'POST',
             dataType: 'json',
-            data: {
-                'message': message
-            },
+            data: message,
             success: function (response) {
                 if (response.status == 'OK') {
-                    socket.emit('message', {
-                        'message': message
-                    });
-                    $('#message').val('');
+                    socket.emit('message', message);
                 }
             }
         });
     });
-    socket.on('send', function (data) {
-        var message = data.message;
-        var html = "<div class='msg'><div class='user'>" + "</div><div class='txt'>" + message + "</div></div>";
+    socket.on('UploadNotification', function (data) {
+        var html = "<div class='msg'>" + "</div><div class='txt'>" + data + "</div>";
         $('.messages').append(html);
-    });
-    socket.on('count_chatters', function (data) {
-        if (data.action == 'increase') {
-            chatter_count++;
-        } else {
-            chatter_count--;
-        }
-        $('.chat-info').text("There are currently " + chatter_count + " people in the chat room");
     });
 });
